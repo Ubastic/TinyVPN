8b2ff4c3ece90e96a815d2cc9c09ac67d32c6e46
wangtai <8306734@qq.com>
2017-11-29 18:48:27 -0800

    Remove libtins

--- src/client.cpp
+++ src/client.cpp
@@ -10,7 +10,7 @@
 namespace vpn {
 
 Client::Client(const std::string& addr, int port)
-    : _socket(Socket::IPv4, Socket::UDP), _epoll(), _tun(),
+    : _socket(Socket::IPv4, Socket::UDP), _epoll(), _tun("192.168.10.111"),
     _srv_port(port), _srv_addr(addr) {
     assert(_epoll.add_read_event(_tun.fd()) == 0);
     assert(_epoll.add_read_event(_socket.fd()) == 0);
@@ -27,10 +27,23 @@ void Client::run() {
                 int nread = _tun.read(buf, sizeof(buf));
                 assert(nread != -1);
                 assert(_socket.sendto(buf, nread, _srv_addr, _srv_port) == nread);
+#ifdef DEBUG
+                std::cout << "recvfrom: " << _tun.ip()
+                    << "sendto: " << _srv_addr << ":" << _srv_port << std::endl << std::endl;
+#endif
             } else if (event.data.fd == _socket.fd()) {
-                int nread = _socket.recvfrom(buf, sizeof(buf), nullptr, nullptr);
+                struct sockaddr_in sock;
+                socklen_t len = sizeof(sock);
+                int nread = _socket.recvfrom(buf, sizeof(buf), (struct sockaddr*)&sock, &len);
                 assert(nread != -1);
                 assert(_tun.write(buf, nread) == nread);
+
+                char buf[1230];
+                inet_ntop(AF_INET, &sock.sin_addr, buf, sizeof(buf));
+#ifdef DEBUG
+                std::cout << "recvfrom" << buf << ":" << ntohs(sock.sin_port)
+                    << "sendto: " << _tun.ip() << std::endl << std::endl;
+#endif
             } else {
                 /* nerver do this */
                 assert(false);
